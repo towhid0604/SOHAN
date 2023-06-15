cde7bd5d425b44472f29e78fb8c6a63fa5069fcb
Mohammed Towhidul Islam <136007237+towhid0604@users.noreply.github.com>
2023-06-16 01:40:40 +0600

    Add files via upload

---
+++ prb_1_10.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+int main(){
+
+	/*
+	printf("Problem 1000\n");
+	printf("Hello World!");
+	*/
+
+	/*
+	printf("Problem 1001\n");
+	int A,B,X;
+	printf("Enter Your Input:");
+	scanf("%d %d",&A,&B);
+	X=A+B;
+	printf("X= %d",X);
+	*/
+
+	/*
+	printf("Problem 1002\n");
+	double R,A;
+	printf("Enter Your Input:");
+	scanf("%lf",&R);
+	A= 3.14159*R*R;
+	printf("%lf",A);
+	*/
+
+	/*
+	printf("Problem 1003\n");
+	int A,B,SOMA;
+	printf("Enter Your Input:");
+	scanf("%d %d",&A,&B);
+	SOMA = A+B;
+	printf("SOMA = %d",SOMA);
+	*/
+
+
+	/*
+	printf("Problem 1004\n");
+	int A,B,PROD;
+	printf("Enter Your Input:");
+	scanf("%d %d",&A,&B);
+	PROD = A*B;
+	printf("SOMA = %d",PROD);
+	*/
+
+	
+	/*
+	printf("Problem 1005\n");
+	float A,B,MEDIA;
+	printf("Enter Your Input:");
+	scanf("%f %f",&A,&B);
+	MEDIA = ((A*3.5+B*7.5)/(3.5+7.5));
+	printf("MEDIA = %.5f",MEDIA);
+	*/
+
+	/*
+	printf("Problem 1006\n");
+	float A,B,C,MEDIA;
+	printf("Enter Your Input:");
+	scanf("%f %f %f",&A,&B,&C);
+	MEDIA = ((A*2+B*3+C*5)/(2+3+5));
+	printf("MEDIA = %.1f",MEDIA);
+	*/
+
+	/*
+	printf("Problem 1007\n");
+	int A,B,C,D,dif;
+	printf("Enter Your Input:\n");
+	scanf("%d %d %d %d",&A,&B,&C,&D);
+	dif=((A*B)-(C*D));
+	printf("DIFERENCE = %d",dif);
+	*/
+
+	/*
+	printf("Problem 1008\n");
+	int number,perh;
+	float month,salary;
+	printf("Enter Your Number:\n");
+	scanf("%d %d %f",&number,&perh,&month);
+	salary=perh*month;
+	printf("Number = %d\n",number);
+	printf("SALARY = U$ %.2f\n",salary);
+	*/
+
+	/*
+	printf("Problem 1009\n");
+	char name;
+	double total,sold,value;
+	printf("Enter Saler Name,value & sold product:\n");
+	scanf("%s %lf %lf",&name,&value,&sold);
+	total=value+sold*.15;
+	printf("Total = R$ %.2lf\n",total);
+	*/
+	
+
+	/*
+	printf("Problem 1010\n");
+	int a,b;
+	float c,total;
+	printf("Enter The value:\n");
+	scanf("%d %d %f",&a,&b,&c);
+	total=b*c;
+	scanf("%d %d %f",&a,&b,&c);
+	//total+=b*c;
+	total = total+(b*c);
+	printf("Valor a pagar : R$ %.2f\n",total);
+	*/
+
+
+
+
+	return 0;
+}
